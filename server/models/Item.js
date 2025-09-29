aea5a79a1d6ab6d954dac16ca33c0b5ba0957480
mayur-sehgal <mayursehgal718@gmail.com>
2025-09-30 01:01:46 +0530

    Add te server side funcionalities

---
+++ server/models/Item.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const ItemsSchema = new mongoose.Schema({
+    name: 
+    { type: String, default: 'No Name' },
+
+    userId: 
+    { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    
+    description: 
+    { type: String, default: 'Withouth description' },
+    
+    type:
+    { type: String, enum: ['Lost', 'Found'], required: true },
+    
+    location:
+    {type: String, required: true},
+    
+    date: 
+    { type: String, required: true },
+
+    number: { type: String, required: true },
+    
+    img: [
+        {
+            type: String,
+            default:
+                'https://i.ibb.co/DpZ3qy2/Untitled-design-10.png',
+        },
+    ],
+    status:{
+        type:String, enum: ['Pending', 'Approved',"Rejected"],required:true,default:"Pending"
+    },
+    createdAt: { type: Date, default: Date.now },
+})
+
+const Item = mongoose.model('Item', ItemsSchema)
+export default Item
