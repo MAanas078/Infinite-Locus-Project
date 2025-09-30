149d669fe7d164b092e7d8e4e01853c0d0e5e277
Mohd Aanas <131160846+MAanas078@users.noreply.github.com>
2025-09-30 10:43:33 +0530

    Update ItemRoutes.js

--- server/routes/ItemRoutes.js
+++ server/routes/ItemRoutes.js
@@ -3,6 +3,8 @@ import createItem from '../controllers/Items/CreateItem.js'
 import { validateJWT } from '../middlewares/validateToken.js'
 import {requireModerator} from "../middlewares/requireModerator.js"
 import getAllItems from '../controllers/Items/getAllItems.js'
+import getAllItemsAdmin from '../controllers/Items/getAllItemsAdmin.js'
+
 import getItemById from '../controllers/Items/getItemById.js'
 import updateItem from '../controllers/Items/updateItem.js'
 import deleteItem from '../controllers/Items/deleteItem.js'
@@ -11,9 +13,9 @@ import RejectItems from '../controllers/Items/RejectItem.js'
 const router = express.Router()
 
 router.post('/newItem',validateJWT, createItem)
-router.get('/get-items',validateJWT,requireModerator, getAllItems)
+router.get('/get-items',validateJWT,requireModerator, getAllItemsAdmin)
 router.get('/:id', getItemById)
-router.get('/', getAllItems)
+router.get('/',getAllItems )
 router.put('/update/:id',validateJWT, updateItem)
 router.delete('/delete/:id', deleteItem)
 router.patch('/:id/approved',validateJWT, requireModerator,ApproveItems)
