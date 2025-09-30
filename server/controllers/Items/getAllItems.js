2cea8348984c947db10f4057c77679f849d557c2
Mohd Aanas <131160846+MAanas078@users.noreply.github.com>
2025-09-30 10:44:02 +0530

    Update getAllItems.js

--- server/controllers/Items/getAllItems.js
+++ server/controllers/Items/getAllItems.js
@@ -2,18 +2,18 @@ import Item from '../../models/Item.js'
 
 const getAllItems = async (req, res) => {
     try {
-        const items = await Item.find();
+        const items = await Item.find({ status: "Approved" });
 
         if (items.length > 0) {
             return res.json({ items })
         } else {
-            return res.status(204).json({ ok: false, msg: 'No items in DB' })
+            return res.status(204).json({ ok: false, msg: 'No Approved items in DB' })
         }
     } catch (error) {
         console.log(error)
-        return res.status(404).json({
+        return res.status(500).json({
             ok: false,
-            msg: 'An error occured, contact with admin',
+            msg: 'An error occurred, contact admin',
         })
     }
 }
